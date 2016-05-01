5cddc380c2b25ff4f1da3a56e20c849782d57244
Jim Hildensperger <jhildensperger@gmail.com>
2016-05-01 18:59:25 +0200

    Hook up contact form Updates to copy

---
+++ js/contact.js
@@ -0,0 +1,50 @@
+$(function() {
+  "use strict";
+  $("input, textarea").jqBootstrapValidation({
+        preventSubmit: true,
+        submitSuccess: function($form, event) {
+            event.preventDefault(); // prevent default submit behaviour
+            // get values from FORM
+
+            $(":button").innerHTML = 'sending...';
+
+            var name = $("input#_name").val();
+            var email = $("input#_replyto").val();
+            var message = $("textarea#_message").val();
+
+            var xhr = new XMLHttpRequest();
+            xhr.open('POST', 'http://formspree.io/contact@rightshiftservices.com', true);
+            xhr.setRequestHeader("Accept", "application/json");
+            xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+
+            xhr.send( "name=" + name + "&email=" + email + "&message=" + message);
+            xhr.onloadend = function (res) {
+              if (res.target.status === 200) {
+                    $('#success').html("<div class='alert alert-success'>");
+                    $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append("</button>");
+                    $('#success > .alert-success').append("<strong>Your message has been sent. </strong>");
+                    $('#success > .alert-success').append('</div>');
+                    $('#contactForm').trigger("reset");
+              } else {
+                    $('#success').html("<div class='alert alert-danger'>");
+                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;").append("</button>");
+                    $('#success > .alert-danger').append("<strong>Sorry, it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append('</div>');
+              }
+            };
+        },
+        filter: function() {
+            return $(this).is(":visible");
+        },
+    });
+
+    $("a[data-toggle=\"tab\"]").click(function(e) {
+        e.preventDefault();
+        $(this).tab("show");
+    });
+});
+
+/*When clicking on Full hide fail/success boxes */
+$('#name').focus(function() {
+    $('#success').html('');
+});
